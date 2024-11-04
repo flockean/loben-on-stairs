8bfdef3fba4e99b7cef4df5290fdcca08cd6100e
flockean <lucas.buchholz1@gmail.com>
2024-11-04 17:32:17 +0100

    feat: routing / views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './styles/App.css';
-import AuthForm from "./components/login-page";
+import AuthForm from "./components/AuthFormView";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import UploadView from "./components/UploadView";
+import React from "react";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AuthForm/>,
+    },
+    {
+        path: "/upload",
+        element: <UploadView/>
+    }
+])
 
 function App() {
   return (
-    <AuthForm />
+      <RouterProvider router={router}/>
   );
 }
 
